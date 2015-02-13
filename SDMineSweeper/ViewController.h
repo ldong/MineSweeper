0d1395e01820ccb1eee561935a7ceca8d0296bfd
haha <you@example.com>
2015-02-12 18:43:36 -0800

    Initial Commit

---
+++ SDMineSweeper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDMineSweeper
+//
+//  Created by sdong on 2/12/15.
+//  Copyright (c) 2015 SD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SDMineSweeper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SDMineSweeper
+//
+//  Created by sdong on 2/12/15.
+//  Copyright (c) 2015 SD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
